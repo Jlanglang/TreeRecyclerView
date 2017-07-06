ae0a74470185c3a1264ba3d378cf896e22ee73e2
jlanglang <347891332@qq.com>
2017-07-06 22:01:55 +0800

    1.添加craetItem的工厂方法.

---
+++ app/src/main/java/com/baozi/treerecyclerview/SImpleRecyclerAdapter.java
@@ -0,0 +1,10 @@
+package com.baozi.treerecyclerview;
+
+import com.baozi.treerecyclerview.adpater.BaseRecyclerAdapter;
+
+/**
+ * Created by baozi on 2017/6/10.
+ */
+
+public class SImpleRecyclerAdapter extends BaseRecyclerAdapter {
+}
